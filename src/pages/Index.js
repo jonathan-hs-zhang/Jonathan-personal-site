ee1b2e53a14b1303a40c54ccd1a6da9a4047d3d1
jonathan <73516742+jonathan-hs-zhang@users.noreply.github.com>
2023-11-22 13:34:50 -0800

    test version

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,7 +5,7 @@ import Main from '../layouts/Main';
 
 const Index = () => (
   <Main
-    description={"Michael D'Angelo's personal website. New York based Stanford ICME graduate, "
+    description={"Jonathan Zhang's personal website. Electrical Engineer UBC trained , "
     + 'VP of Engineering at Smile Identity, co-founder of Arthena and Matroid, and YC Alumni.'}
   >
     <article className="post" id="index">
