8545a6ab7a42e8b666e617ca06f54c468e1f62da
jonathan <73516742+jonathan-hs-zhang@users.noreply.github.com>
2023-11-22 15:58:52 -0800

    version2

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -30,7 +30,7 @@ const SideBar = () => (
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">&copy; Jonathan Zhang <Link to="/">mldangelo.com</Link>.</p>
     </section>
   </section>
 );
