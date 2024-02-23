08c8754b47b1d73aae334c808b25c634973b0538
jonathan <ZHSzhs126142@outlook.com>
2024-02-23 03:20:45 -0800

    Update App.test.js

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -70,15 +70,15 @@ describe('renders the app', () => {
     expect(window.location.pathname).toBe('/resume');
   });
 
-  it('can navigate to /projects', async () => {
+  it('can navigate to /life', async () => {
     expect.assertions(3);
     const contactLink = document.querySelector('#header > nav > ul > li:nth-child(3) > a');
     expect(contactLink).toBeInTheDocument();
     await act(async () => {
       await contactLink.click();
     });
-    expect(document.title).toContain('Projects |');
-    expect(window.location.pathname).toBe('/projects');
+    expect(document.title).toContain('Life |');
+    expect(window.location.pathname).toBe('/life');
   });
 
   it('can navigate to /stats', async () => {
