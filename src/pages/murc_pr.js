28449b45a3a397c0f4a8659f1ab795b95712b423
jonathan <ZHSzhs126142@outlook.com>
2024-02-23 03:54:57 -0800

    Update murc_pr.js

--- src/pages/murc_pr.js
+++ src/pages/murc_pr.js
@@ -11,20 +11,20 @@ const murc = () => (
     <article className="post" id="murc">
       <header>
         <div className="title">
-          <h2><Link to="/murc">3D printer temperature controller presentation</Link></h2>
+          <h2><Link to="/murc_pr">3D printer temperature controller presentation</Link></h2>
         </div>
       </header>
       <div className="details">
         <div className="description">
           <p> Kezi Li (on the right), a UBC PhD student, who mentored me this research.</p>
           <p><a href="https://www.linkedin.com/in/kezi-li/">His linkedin webpage</a></p>
-          <img src="/images/murc/kezi.jpeg" alt="1" width="450" height="600" />
+          <img src={`${process.env.PUBLIC_URL}${'/images/murc/kezi.jpeg'}`} alt="1" width="600" height="800" />
           <p> Our design can be seen in the poster.</p>
           <p> Presented this poster at MURC finally on March 28, 2023. </p>
-          <img src="/images/murc/poster.png" alt="1" width="800" height="600" />
+          <img src={`${process.env.PUBLIC_URL}${'/images/murc/poster.png'}`} alt="1" width="800" height="600" />
           <p> There are some pictures of me presenting at MURC. </p>
-          <img src="/images/murc/audience1.jpeg" alt="1" width="800" height="600" />
-          <img src="/images/murc/audience2.jpeg" alt="1" width="800" height="600" />
+          <img src={`${process.env.PUBLIC_URL}${'/images/murc/audience1.jpeg'}`} alt="1" width="800" height="600" />
+          <img src={`${process.env.PUBLIC_URL}${'/images/murc/audience2.jpeg'}`} alt="1" width="800" height="600" />
         </div>
       </div>
     </article>
