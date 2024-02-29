53cf02c2bc23a2066eee10f4644b2f01b9ce8e5f
jonathan <ZHSzhs126142@outlook.com>
2024-02-29 02:38:07 -0800

    Create graduation.js

---
+++ src/pages/graduation.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Main from '../layouts/Main';
+
+const graduation = () => (
+  <Main
+    title="graduation"
+    description="info about graduation"
+  >
+    <article className="post" id="graduation">
+      <header>
+        <div className="title">
+          <h2><Link to="/graduation">All my graduation inforamtion </Link></h2>
+        </div>
+      </header>
+      <div className="details">
+        <div className="description">
+          <p> Got me degree on May 2023</p>
+          <img src={`${process.env.PUBLIC_URL}${'/images/degree.pdf'}`} alt="1" />
+          <p> Some photos taken with my best friends and in front of IKB library </p>
+          <img src={`${process.env.PUBLIC_URL}${'/images/grad1.jpeg'}`} alt="1" />
+          <img src={`${process.env.PUBLIC_URL}${'/images/grad2.jpeg'}`} alt="1" />
+          <img src={`${process.env.PUBLIC_URL}${'/images/grad3.jpeg'}`} alt="1" />
+          <p> Here is my program completion letter attached.</p>
+          <img src={`${process.env.PUBLIC_URL}${'/images/program_completion_letter.pdf'}`} alt="1" />
+          <p> Below is my ubc official transcript. </p>
+          <img src={`${process.env.PUBLIC_URL}${'/images/transcript1.pdf'}`} alt="1" />
+          <img src={`${process.env.PUBLIC_URL}${'/images/transcript2.pdf'}`} alt="1" />
+          <img src={`${process.env.PUBLIC_URL}${'/images/transcript3.pdf'}`} alt="1" />
+        </div>
+      </div>
+    </article>
+  </Main>
+);
+
+export default graduation;
